386d9c84dd664b12d87aee8078eddb2824068ade
Andrei A <aaiordac@users.noreply.github.com>
2021-12-13 08:48:07 +0200

    Use Authentication header for passing the GitHub token, as per https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param/

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-alert */
 const browser = window.browser || window.chrome;
 
-const tokenName = 'gscsv_personal_token';
+const tokenName = "gscsv_personal_token";
 
 const textMatchHeaders = new Headers();
-textMatchHeaders.append('Accept', 'application/vnd.github.v3.text-match+json');
+textMatchHeaders.append("Accept", "application/vnd.github.v3.text-match+json");
 
-const downloadButton = document.createElement('button');
-const downloadText = browser.i18n.getMessage('downloadBtn') || 'Download';
-const noTokenText = browser.i18n.getMessage('noTokenBtn') || 'Set a valid token in config';
+const downloadButton = document.createElement("button");
+const downloadText = browser.i18n.getMessage("downloadBtn") || "Download";
+const noTokenText =
+  browser.i18n.getMessage("noTokenBtn") || "Set a valid token in config";
 
 /**
  * Returns extension saved config
@@ -16,9 +17,12 @@ const noTokenText = browser.i18n.getMessage('noTokenBtn') || 'Set a valid token 
  * @param {function} [callback=(token) => {}] Callback function
  */
 function getToken(callback = (token) => {}) {
-  browser.storage.sync.get({
-    [tokenName]: ''
-  }, config => callback(config[tokenName]));
+  browser.storage.sync.get(
+    {
+      [tokenName]: "",
+    },
+    (config) => callback(config[tokenName])
+  );
 }
 
 /**
@@ -27,9 +31,9 @@ function getToken(callback = (token) => {}) {
  * @param {String} [content=''] File content
  * @param {string} [filename='download']
  */
-function downloadCsvFile(content = '', filename = 'download') {
-  const blob = new Blob([content], { type: 'text/csv;charset=utf-8' });
-  const link = window.document.createElement('a');
+function downloadCsvFile(content = "", filename = "download") {
+  const blob = new Blob([content], { type: "text/csv;charset=utf-8" });
+  const link = window.document.createElement("a");
   const fileName = `${filename}_${new Date().getTime()}.csv`;
 
   link.href = window.URL.createObjectURL(blob);
@@ -37,7 +41,7 @@ function downloadCsvFile(content = '', filename = 'download') {
   document.body.appendChild(link);
 
   downloadButton.disabled = false;
-  downloadButton.querySelector('span').textContent = downloadText;
+  downloadButton.querySelector("span").textContent = downloadText;
 
   link.click();
   document.body.removeChild(link);
@@ -50,45 +54,51 @@ function downloadCsvFile(content = '', filename = 'download') {
  */
 function generateCsvFile(data) {
   if (data) {
-    let csvContent = 'Organization,Repository,Repository URL,Result file URL,Results\n';
+    let csvContent =
+      "Organization,Repository,Repository URL,Result file URL,Results\n";
 
-    data.forEach(item => {
-      const matched = item.text_matches.map(match => match.fragment.replace(/(?:\r\n|\r|\n|,|\s{2,})/g, ' ').toString());
-      csvContent += `${item.repository.owner.login},${item.repository.name},${encodeURI(item.repository.html_url)},${encodeURI(item.html_url)},${matched}\n`;
+    data.forEach((item) => {
+      const matched = item.text_matches.map((match) =>
+        match.fragment.replace(/(?:\r\n|\r|\n|,|\s{2,})/g, " ").toString()
+      );
+      csvContent += `${item.repository.owner.login},${
+        item.repository.name
+      },${encodeURI(item.repository.html_url)},${encodeURI(
+        item.html_url
+      )},${matched}\n`;
     });
 
-    downloadCsvFile(csvContent, 'search_results');
+    downloadCsvFile(csvContent, "search_results");
   } else {
-    alert('No results found');
+    alert("No results found");
   }
 }
 
 /**
  * Searches through code via GitHub API
  *
- * @param {string} [token='']
  * @param {number} [page=1]
  * @param {number} [resultsCount=0]
  * @param {Array} [items=[]]
  * @returns
  */
-async function search(token = '', page = 1, resultsCount = 0, items = []) {
-  const apiUrl = new URL('https://api.github.com/search/code');
-  const queryParams = new URLSearchParams(window.location.search).get('q') || '';
+async function search(page = 1, resultsCount = 0, items = []) {
+  const apiUrl = new URL("https://api.github.com/search/code");
+  const queryParams =
+    new URLSearchParams(window.location.search).get("q") || "";
 
-  apiUrl.searchParams.set('q', queryParams);
-  apiUrl.searchParams.set('page', page);
-  apiUrl.searchParams.set('access_token', token);
+  apiUrl.searchParams.set("q", queryParams);
+  apiUrl.searchParams.set("page", page);
 
   try {
     const response = await fetch(apiUrl, {
-      method: 'GET',
+      method: "GET",
       headers: textMatchHeaders,
-      cache: 'default'
+      cache: "default",
     });
 
     if (response.status === 403) {
-      alert('Rate limit reached. Downloading only available items');
+      alert("Rate limit reached. Downloading only available items");
       return generateCsvFile(items);
     }
 
@@ -99,8 +109,9 @@ async function search(token = '', page = 1, resultsCount = 0, items = []) {
 
     // Iterate through pages
     if (resultsCount < data.total_count) {
-      search(token, page + 1, resultsCount, items);
-    } else { // No more pages, export file
+      search(page + 1, resultsCount, items);
+    } else {
+      // No more pages, export file
       generateCsvFile(items);
     }
   } catch (event) {
@@ -114,11 +125,13 @@ async function search(token = '', page = 1, resultsCount = 0, items = []) {
  * @param {Event} event Event
  */
 function onDownload(event) {
-  getToken(token => {
-    downloadButton.querySelector('span').textContent = browser.i18n.getMessage('downloadingStatus') || 'Downloading...';
+  getToken((token) => {
+    downloadButton.querySelector("span").textContent =
+      browser.i18n.getMessage("downloadingStatus") || "Downloading...";
     downloadButton.disabled = true;
+    textMatchHeaders.append("Authorization", "token " + token);
 
-    search(token);
+    search();
   });
 }
 
@@ -126,7 +139,7 @@ function onDownload(event) {
  * Sends an event to open options page
  */
 function openOptionsPage() {
-  browser.runtime.sendMessage({ action: 'openOptionsPage' });
+  browser.runtime.sendMessage({ action: "openOptionsPage" });
 }
 
 /**
@@ -134,24 +147,24 @@ function openOptionsPage() {
  * to export results to CSV
  */
 function addDownloadButton() {
-  getToken(token => {
-    const buttonsDiv = document.querySelector('#code_search_results');
+  getToken((token) => {
+    const buttonsDiv = document.querySelector("#code_search_results");
 
     if (!buttonsDiv) {
       return;
     }
 
-    downloadButton.classList.add('btn', 'btn-sm', 'mt-4');
+    downloadButton.classList.add("btn", "btn-sm", "mt-4");
 
     if (token) {
-      downloadButton.addEventListener('click', onDownload);
+      downloadButton.addEventListener("click", onDownload);
       downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${downloadText}</span><svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="14" height="14" style="margin-left:5px;"><g fill="none" fill-rule="evenodd"><g fill="#000"><path d="m4 6h3v-6h2v6h3l-4 4-4-4zm11-4h-4v1h4v8h-14v-8h4v-1h-4c-0.55 0-1 0.45-1 1v9c0 0.55 0.45 1 1 1h5.34c-0.25 0.61-0.86 1.39-2.34 2h8c-1.48-0.61-2.09-1.39-2.34-2h5.34c0.55 0 1-0.45 1-1v-9c0-0.55-0.45-1-1-1z"/></g></g></svg></div>`;
     } else {
-      downloadButton.addEventListener('click', openOptionsPage);
+      downloadButton.addEventListener("click", openOptionsPage);
       downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${noTokenText}</span></div>`;
     }
 
-    buttonsDiv.insertAdjacentElement('afterbegin', downloadButton);
+    buttonsDiv.insertAdjacentElement("afterbegin", downloadButton);
   });
 }
 
@@ -159,7 +172,7 @@ function addDownloadButton() {
  * Initialize library
  */
 function init() {
-  const observeTarget = document.querySelector('body');
+  const observeTarget = document.querySelector("body");
   const observer = new MutationObserver(addDownloadButton);
 
   observer.observe(observeTarget, { childList: true });
